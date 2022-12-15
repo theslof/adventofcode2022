298a15a1f31cb49616a5571f851392d0184764e6
Jonas Theslöf <theslof@gmail.com>
2022-12-15 16:22:06 +0100

    Day 15 Part 2

--- day15.ts
+++ day15.ts
@@ -32,30 +32,6 @@ function part1(lines: string[]): number {
     return [{x: Sx, y: Sy, tile: Tile.SENSOR, distance: Dx + Dy}, {x: Bx, y: By, tile: Tile.BEACON, distance: Dx + Dy}]
   }).flat()
 
-  // const map: Tile[][] = Array.from({length: yMax - yMin + 1}, (_, y) => Array.from({length: xMax - xMin + 1}, (_, x) => {
-  //   let tile = Tile.EMPTY
-  //   for (let i = 0; i < points.length; i++) {
-  //     const point = points[i]
-  //     const Py = point.y - yMin
-  //     const Px = point.x - xMin
-  //     const Dy = Math.abs(Py - y)
-  //     const Dx = Math.abs(Px - x)
-  //
-  //     if (y === Py && x === Px) {
-  //       return point.tile
-  //     }
-  //
-  //     if (point.tile !== Tile.SENSOR) continue
-  //
-  //     if (Dy + Dx <= point.distance) {
-  //       tile = Tile.NOT_POSSIBLE
-  //     }
-  //   }
-  //   return tile
-  // }))
-
-  // mapToBitMap(map)
-
   let part1 = 0
   for (let x = xMin; x <= xMax; x++) {
     const y = 2000000
@@ -84,7 +60,43 @@ function part1(lines: string[]): number {
   return part1
 }
 
+function part2(lines: string[]): number {
+  const points: Point[] = lines.map((line) => {
+    const match = line.match(/Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/)
+    const Sx = Number(match[1])
+    const Sy = Number(match[2])
+    const Bx = Number(match[3])
+    const By = Number(match[4])
+    const Dx = Math.abs(Sx - Bx)
+    const Dy = Math.abs(Sy - By)
+    return {x: Sx, y: Sy, tile: Tile.SENSOR, distance: Dx + Dy}
+  })
+
+  const MAX = 4000000
+
+  function findSensor(y: number, x: number): boolean {
+    if (y < 0 || y > MAX || x < 0 || x > MAX) return true
+    return !!points.find(point => Math.abs(y - point.y) + Math.abs(x - point.x) <= point.distance)
+  }
+
+  for (let i = 0; i < points.length; i++) {
+    const point = points[i]
+    for (let x = point.x, y = point.y - point.distance - 1; y <= point.y; x++, y++) {
+      if (!findSensor(y, x)) return x * 4000000 + y
+    }
+    for (let x = point.x, y = point.y + point.distance + 1; y >= point.y; x++, y--) {
+      if (!findSensor(y, x)) return x * 4000000 + y
+    }
+    for (let x = point.x, y = point.y - point.distance - 1; y <= point.y; x--, y++) {
+      if (!findSensor(y, x)) return x * 4000000 + y
+    }
+    for (let x = point.x, y = point.y + point.distance + 1; y >= point.y; x--, y--) {
+      if (!findSensor(y, x)) return x * 4000000 + y
+    }
+  }
+  return -1
+}
+
 
 console.log('Part 1:', part1(readData(15).filter(f => f)))
-// console.log('Part 2:', part2(readData(15).filter(f => f)))
-// Still need to figure out a good performing algorithm for finding the missing beacon for larger data sets.
+console.log('Part 2:', part2(readData(15).filter(f => f)))
